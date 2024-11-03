31b82e38b22e31e1a17ee3f3c992bc010b4dd90f
thakareganesh <thakareganesh803@gmail.com>
2024-11-03 18:04:12 +0530

    tuple practice

--- Code/Problems (Class)/November 2024 - Practice/2024 Nov 03 (Tuple)/tuples_practice_Ex1.py
+++ Code/Problems (Class)/November 2024 - Practice/2024 Nov 03 (Tuple)/tuples_practice_Ex1.py
@@ -4,3 +4,19 @@
 t1 = (1,2,3,2,5)
 print(type(t1))
 print(f"tuples: {t1}")
+print("=" * 40) # --> used for decoration purpose
+
+# Indexing allowed in tuples due to they are ORDERED
+i1 = t1.index(3)
+i2 = t1.index(5)
+i3 = t1.index(2)
+print(f"5 is present at index: {i2}")
+print(f"3 is present at index: {i1}")
+print(f"2 is present at index: {i3}")
+
+# slicing is possible 
+print(f"Return all tuples: {t1[:]}")
+print(f"Return all tuples: {t1[::]}")
+print(f"Return all tuples: {t1[::-1]}")
+
+# Operation
