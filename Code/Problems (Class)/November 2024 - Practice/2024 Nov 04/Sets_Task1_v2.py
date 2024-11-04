75a2187edf9bcc97620032370ee297d5acb2860a
thakareganesh <thakareganesh803@gmail.com>
2024-11-04 23:12:35 +0530

    added sets task 1 v2 problem

---
+++ Code/Problems (Class)/November 2024 - Practice/2024 Nov 04/Sets_Task1_v2.py
@@ -0,0 +1,14 @@
+# Sets Task 1:
+
+
+print("Brand Factory")
+# Takes jubilee_hills empty set
+jubilee_hills = set()
+# Adding values in jubilee hills
+jubilee_hills.add("Allen Solly")
+jubilee_hills.add("Wrangler")
+jubilee_hills.add("Pepe Jeans")
+jubilee_hills.add("Allen Solly")
+jubilee_hills.add("Allen Solly")
+jubilee_hills.add("Allen Solly")
+print(f"Jubilee Hills Brands: {jubilee_hills}")
