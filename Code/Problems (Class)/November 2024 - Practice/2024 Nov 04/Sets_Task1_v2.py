5ea3227d56e45132bce7a39fe3faae62f3fc4bc5
thakareganesh <thakareganesh803@gmail.com>
2024-11-06 08:06:08 +0530

    Not resolved yet

--- Code/Problems (Class)/November 2024 - Practice/2024 Nov 04/Sets_Task1_v2.py
+++ Code/Problems (Class)/November 2024 - Practice/2024 Nov 04/Sets_Task1_v2.py
@@ -2,13 +2,33 @@
 
 
 print("Brand Factory")
-# Takes jubilee_hills empty set
+# taking empty set 1 for Jubilee Hills branch
 jubilee_hills = set()
 # Adding values in jubilee hills
 jubilee_hills.add("Allen Solly")
 jubilee_hills.add("Wrangler")
 jubilee_hills.add("Pepe Jeans")
+jubilee_hills.add("Levi's")
+jubilee_hills.add("Flying Machine")
 jubilee_hills.add("Allen Solly")
-jubilee_hills.add("Allen Solly")
-jubilee_hills.add("Allen Solly")
+jubilee_hills.add("Spykar")
+jubilee_hills.add("VanVeusen")
 print(f"Jubilee Hills Brands: {jubilee_hills}")
+
+# taking empty set 2 for BegumPet branch
+begumPet = set()
+begumPet.add("Allen Solly")
+begumPet.add("Mufti")
+begumPet.add("Wrangler")
+begumPet.add("Pepe Jeans")
+begumPet.add("Lee")
+begumPet.add("Van Heusen")
+begumPet.add("Monte Carlo")
+begumPet.add("Flying Machine")
+print(f"BegumPet Brands: {begumPet}")
+
+# common brands in both places
+common_brands = jubilee_hills.intersection(begumPet)
+print(f"Following are the Brands which are common brands available in both place: \n{common_brands}")
+
+#
