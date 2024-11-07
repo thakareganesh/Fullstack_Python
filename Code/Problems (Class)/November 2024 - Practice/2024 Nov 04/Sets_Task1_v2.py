1ec8984332f4596644dbbf89b9507d9c881ca171
thakareganesh <thakareganesh803@gmail.com>
2024-11-07 08:35:19 +0530

    Completed Set Task 1 V2

--- Code/Problems (Class)/November 2024 - Practice/2024 Nov 04/Sets_Task1_v2.py
+++ Code/Problems (Class)/November 2024 - Practice/2024 Nov 04/Sets_Task1_v2.py
@@ -2,6 +2,7 @@
 
 
 print("Brand Factory")
+print()
 # taking empty set 1 for Jubilee Hills branch
 jubilee_hills = set()
 # Adding values in jubilee hills
@@ -14,7 +15,7 @@ jubilee_hills.add("Allen Solly")
 jubilee_hills.add("Spykar")
 jubilee_hills.add("VanVeusen")
 print(f"Jubilee Hills Brands: {jubilee_hills}")
-
+print()
 # taking empty set 2 for BegumPet branch
 begumPet = set()
 begumPet.add("Allen Solly")
@@ -26,9 +27,19 @@ begumPet.add("Van Heusen")
 begumPet.add("Monte Carlo")
 begumPet.add("Flying Machine")
 print(f"BegumPet Brands: {begumPet}")
-
-# common brands in both places
+print()
+# Q.1) Display the common brands available in two places
 common_brands = jubilee_hills.intersection(begumPet)
 print(f"Following are the Brands which are common brands available in both place: \n{common_brands}")
+print()
 
-#
+# Q.2) Display the brands which are available only in Begumpet branch
+only_BegumPet = begumPet.difference(jubilee_hills)
+print(f"Follwing are the brands which are only available in BegumPet: {only_BegumPet}")
+print()
+
+# Q.3) Display the unique brands which are available in Jubilee Hills and BegumPet Branch.
+unique_in_both = begumPet.symmetric_difference(jubilee_hills)
+print("Following are the brands which are available in Jubilee Hills and BegumPet Branch except common:")
+print(unique_in_both)
+print()
