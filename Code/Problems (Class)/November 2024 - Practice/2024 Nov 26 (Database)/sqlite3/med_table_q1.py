f6595b5fbd1dc34c4dedbff12e89b4dbbe8fcf21
thakareganesh <thakareganesh803@gmail.com>
2024-12-01 19:06:14 +0530

    minor change

--- Code/Problems (Class)/November 2024 - Practice/2024 Nov 26 (Database)/sqlite3/med_table_q1.py
+++ Code/Problems (Class)/November 2024 - Practice/2024 Nov 26 (Database)/sqlite3/med_table_q1.py
@@ -4,7 +4,8 @@ import sqlite3 as sql
 con = sql.connect("Amazon")
 print("opened connection")
 cur = con.cursor()
-q = "create table product(MedId int primary key, MedName text, Cost real,Availability int, ExpDate date)"
+q = "create table Medicines(MedId int primary key, MedName text, Cost real,Availability int, ExpDate date)"
 cur.execute(q)
 print("Table created")
 con.close()
+p
